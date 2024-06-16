36bf646059b22079b5210ef7cfc2df74675acfe4
hendisantika <hendisantika@yahoo.co.id>
2024-06-16 15:13:58 +0700

    Add createProduct in ImageGalleryController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/imagegalleryapp/controller/ImageGalleryController.java
+++ src/main/java/id/my/hendisantika/imagegalleryapp/controller/ImageGalleryController.java
@@ -1,11 +1,27 @@
 package id.my.hendisantika.imagegalleryapp.controller;
 
+import id.my.hendisantika.imagegalleryapp.entity.ImageGallery;
 import id.my.hendisantika.imagegalleryapp.service.ImageGalleryService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.math.BigDecimal;
+import java.nio.file.Paths;
+import java.util.Date;
 
 /**
  * Created by IntelliJ IDEA.
@@ -30,6 +46,60 @@ public class ImageGalleryController {
     @GetMapping(value = {"/", "/home"})
     public String addProductPage() {
         return "index";
+    }
+
+    @PostMapping("/image/saveImageDetails")
+    public @ResponseBody ResponseEntity<?> createProduct(@RequestParam("name") String name,
+                                                         @RequestParam("price") BigDecimal price,
+                                                         @RequestParam("description") String description,
+                                                         Model model, HttpServletRequest request,
+                                                         final @RequestParam("image") MultipartFile file) {
+        try {
+            //String uploadDirectory = System.getProperty("user.dir") + uploadFolder;
+            String uploadDirectory = request.getServletContext().getRealPath(uploadFolder);
+            log.info("uploadDirectory:: {}", uploadDirectory);
+            String fileName = file.getOriginalFilename();
+            String filePath = Paths.get(uploadDirectory, fileName).toString();
+            log.info("FileName: {}", file.getOriginalFilename());
+            if (fileName == null || fileName.contains("..")) {
+                model.addAttribute("invalid", "Sorry! Filename contains invalid path sequence \" + fileName");
+                return new ResponseEntity<>("Sorry! Filename contains invalid path sequence " + fileName, HttpStatus.BAD_REQUEST);
+            }
+            String[] names = name.split(",");
+            String[] descriptions = description.split(",");
+            Date createDate = new Date();
+            log.info("Name: {} {}", names[0], filePath);
+            log.info("description: {}", descriptions[0]);
+            log.info("price: {}", price);
+            try {
+                File dir = new File(uploadDirectory);
+                if (!dir.exists()) {
+                    log.info("Folder Created");
+                    dir.mkdirs();
+                }
+                // Save the file locally
+                BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(new File(filePath)));
+                stream.write(file.getBytes());
+                stream.close();
+            } catch (Exception e) {
+                log.info("in catch");
+                e.printStackTrace();
+            }
+            byte[] imageData = file.getBytes();
+            ImageGallery imageGallery = new ImageGallery();
+            imageGallery.setName(names[0]);
+            imageGallery.setImage(imageData);
+            imageGallery.setPrice(price);
+            imageGallery.setDescription(descriptions[0]);
+            imageGallery.setCreateDate(createDate);
+            imageGalleryService.saveImage(imageGallery);
+            log.info("HttpStatus==={}", new ResponseEntity<>(HttpStatus.OK));
+            return new ResponseEntity<>("Product Saved With File - " + fileName, HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.info("Exception: {}", e);
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
 }
